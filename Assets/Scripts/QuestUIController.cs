ae5627297635b106648c5dc272568231dac213dc
ChordArc <flqms710@gmail.com>
2026-05-06 12:30:12 +0900

    test QuestUI

---
+++ Assets/Scripts/QuestUIController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class QuestUIController : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+    [SerializeField] private Image progressFill;
+    [SerializeField, Min(0.1f)] private float questDuration = 5f;
+
+    private Coroutine progressRoutine;
+
+    private void Awake()
+    {
+        ResetProgress();
+
+        if (startButton != null)
+        {
+            startButton.onClick.AddListener(StartQuest);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (startButton != null)
+        {
+            startButton.onClick.RemoveListener(StartQuest);
+        }
+    }
+
+    private void StartQuest()
+    {
+        if (progressRoutine != null)
+        {
+            StopCoroutine(progressRoutine);
+        }
+
+        progressRoutine = StartCoroutine(RunProgress());
+    }
+
+    private IEnumerator RunProgress()
+    {
+        if (startButton != null)
+        {
+            startButton.interactable = false;
+        }
+
+        var elapsed = 0f;
+        SetProgress(0f);
+
+        while (elapsed < questDuration)
+        {
+            elapsed += Time.deltaTime;
+            SetProgress(elapsed / questDuration);
+            yield return null;
+        }
+
+        SetProgress(1f);
+
+        if (startButton != null)
+        {
+            startButton.interactable = true;
+        }
+
+        progressRoutine = null;
+    }
+
+    private void ResetProgress()
+    {
+        SetProgress(0f);
+    }
+
+    private void SetProgress(float value)
+    {
+        if (progressFill != null)
+        {
+            progressFill.fillAmount = Mathf.Clamp01(value);
+        }
+    }
+}
